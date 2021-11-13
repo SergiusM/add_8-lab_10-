0cdc66ca8e2b86160500632f749c61e599244b37
SergiusM <90497067+SergiusM@users.noreply.github.com>
2021-11-13 10:04:11 +0200

    Add files via upload

---
+++ add_8(lab_10).cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "classes.hpp"
+
+int main()
+{
+	mt::Circle circle;
+	int R;
+	mt::Point M;
+	std::cin >> R >> M.x >> M.y;
+	circle.SetM(M);
+	circle.SetR(R);
+	std::cout << "Scuare is " << circle.Square() << std::endl;
+	std::cout << "Perimeter is " << circle.Perimeter();
+}
